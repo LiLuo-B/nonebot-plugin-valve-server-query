c6b0308109b2eea27277baa01047b98039015ce3
LiLuo-B <474115040@qq.com>
2024-05-14 10:32:09 +0800

    修复依赖

--- nonebot_plugin_valve_server_query/__init__.py
+++ nonebot_plugin_valve_server_query/__init__.py
@@ -6,6 +6,7 @@ __plugin_meta__ = PluginMetadata(
     name="nonebot_plugin_valve_server_query",
     description="NoneBot插件，用于查询V社服务器，图片基于html渲染，有权限管理，支持在线更新服务器信息",
     usage="私聊或群里发送消息",
+    type="application",
     homepage="https://github.com/LiLuo-B/nonebot-plugin-valve-server-query",
     config=Config,
     supported_adapters={"~onebot.v11"},
